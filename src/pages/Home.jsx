367b107a5043926bc5dafc953183ba1d47ad667b
unknown <martstepen03@gmail.com>
2025-09-04 20:06:57 +0700

    keetiga

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,23 @@
 import React from "react";
+import { useRef } from "react";
 import Button from "@/components/Button";
 import TextPressure from "@/components/TextPressure";
+import DecryptedText from "@/components/DecryptedText";
+import GlitchText from "@/components/GlitchText";
+import VariableProximity from "@/components/VariableProximity";
+import TextType from "@/components/TextType";
+
 function Home() {
   return (
     <>
-      <div style={{ position: "relative", height: "300px" }}>
-        <TextPressure
-          text="I'M MART"
-          flex={true}
-          alpha={false}
-          stroke={false}
-          width={true}
-          weight={true}
-          italic={true}
-          textColor="#000000"
-          strokeColor="#ff0000"
-          minFontSize={36}
-        />
-      </div>
+      <TextType
+        className="text-red-600 size-72 bg-black"
+        text={["Text typing effect", "for your websites", "Happy coding!"]}
+        typingSpeed={75}
+        pauseDuration={1500}
+        showCursor={true}
+        cursorCharacter="|"
+      />
       <h1>Ini Adalah Home</h1>
       <Button btn="click"></Button>
     </>
